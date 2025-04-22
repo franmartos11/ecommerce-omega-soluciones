f4ff9a665150b7a8a96f26d3d5e2242edb426f36
franmartos11 <franmartos11@gmail.com>
2025-04-21 22:30:44 -0300

    add suspence to contact due to navbar use sereach param

--- ecommerce-omega/src/app/Contacto/page.tsx
+++ ecommerce-omega/src/app/Contacto/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Footer from "../Components/Footer/Footer";
 import Navbar from "../Components/NavigationBar/NavBar";
 import ContactCardImg from "./ContactCardImg";
@@ -5,9 +6,11 @@ import ContactCardImg from "./ContactCardImg";
 export default function Contact() {
     return (
         <div className="bg-white min-h-screen p-8 pb-0 font-[family-name:var(--font-geist-sans)]">
-            <Navbar></Navbar>
-            <ContactCardImg></ContactCardImg>
-            <Footer></Footer>
+            <Suspense fallback={<div className="p-6">Cargando productos...</div>}>
+                <Navbar></Navbar>
+                <ContactCardImg></ContactCardImg>
+                <Footer></Footer>
+            </Suspense>
         </div>
     );
 }
