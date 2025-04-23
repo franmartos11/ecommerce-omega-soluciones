8329297f42799d1ea1eec2738026f7404a412962
franmartos11 <franmartos11@gmail.com>
2025-04-23 19:37:13 -0300

    fix navbar on mobile

--- ecommerce-omega/src/app/Components/NavigationBar/NavBar.tsx
+++ ecommerce-omega/src/app/Components/NavigationBar/NavBar.tsx
@@ -57,7 +57,7 @@ export default function Navbar() {
           <div className="flex items-center gap-1">
             <User className="w-5 h-5" />
             <a href='/LogIn'>
-              <span>Cuenta</span>
+              <span className="hidden md:inline">Cuenta</span>
             </a>
           </div>
         </div>
