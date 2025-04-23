c55d40f985dfd77b98c9c5bf4711d6118c97cc91
franmartos11 <franmartos11@gmail.com>
2025-04-23 16:50:18 -0300

    added paguination and animation on appero of productos

--- ecommerce-omega/src/app/Components/ProductCardGrid/ProductCardGrid.tsx
+++ ecommerce-omega/src/app/Components/ProductCardGrid/ProductCardGrid.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { FC } from "react";
+import { FC, useState } from "react";
 import ProductCard, { ProductCardProps } from "../ProductCard/ProductCard";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export interface Product extends ProductCardProps {
   color: string;
@@ -14,20 +15,49 @@ interface ProductCardGridProps {
 }
 
 const ProductCardGrid: FC<ProductCardGridProps> = ({ products }) => {
+  const [visibleCount, setVisibleCount] = useState(16);
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 16);
+  };
+
+  const visibleProducts = products.slice(0, visibleCount);
+  const hasMore = visibleCount < products.length;
+
   return (
     <div className="w-full px-4 py-6">
-      <h2 className="text-black text-2xl font-semibold mb-4">En Promo</h2>
+      <h2 className="text-black text-2xl font-semibold mb-6 ">En Promo</h2>
+
       <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products.map((product) => (
-          <Link
+        {visibleProducts.map((product) => (
+          <motion.div
             key={product.id}
-            href={`/ProductoDetailPage/${product.id}`}
-            className="block focus:outline-none focus:ring-2 focus:ring-green-400 rounded-lg"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
           >
-            <ProductCard {...product} />
-          </Link>
+            <Link
+              href={`/ProductoDetailPage/${product.id}`}
+              className="block focus:outline-none focus:ring-2 focus:ring-green-400 rounded-lg"
+            >
+              <ProductCard {...product} />
+            </Link>
+          </motion.div>
         ))}
       </div>
+
+      {hasMore && (
+        <div className="flex justify-center mt-10">
+          <motion.button
+            whileTap={{ scale: 0.95 }}
+            whileHover={{ scale: 1.05 }}
+            onClick={handleLoadMore}
+            className="px-6 py-2 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition text-sm font-medium"
+          >
+            Cargar más
+          </motion.button>
+        </div>
+      )}
     </div>
   );
 };
--- ecommerce-omega/src/app/LogIn/page.tsx
+++ ecommerce-omega/src/app/LogIn/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { Mail, Lock } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
