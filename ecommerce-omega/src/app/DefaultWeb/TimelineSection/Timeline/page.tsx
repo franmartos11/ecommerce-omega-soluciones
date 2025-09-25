582f82ce7a5dd90d0f7fded664f3953f9f5ec839
franmartos11 <franmartos11@gmail.com>
2025-09-25 18:38:00 -0300

    Changes on timeline and on Configjson

--- ecommerce-omega/src/app/DefaultWeb/TimelineSection/Timeline/page.tsx
+++ ecommerce-omega/src/app/DefaultWeb/TimelineSection/Timeline/page.tsx
@@ -77,7 +77,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           className="absolute md:left-8 left-8 top-0 overflow-hidden w-[2px] bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200  to-transparent to-[99%]  [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] "
         >
           <motion.div
-            style={{
+            style={{  
               height: heightTransform,
               opacity: opacityTransform,
             }}
